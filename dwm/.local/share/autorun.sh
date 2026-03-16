e35d4b718dc295af108c60681655360d3640de76
Zrgst <zrgst@protonmail.com>
2026-03-16 23:00:30 +0100

    added autorun

---
+++ dwm/.local/share/autorun.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 1. System og D-Bus (Viktig for Arch/systemd)
+dbus-update-activation-environment --systemd --all &
+/usr/lib/mate-polkit/polkit-mate-authentication-agent-1 &
+
+# 2. Skjerm og Grafikk
+# Sørg for at picom starter med b-flagget (bakgrunn)
+picom -b &
+feh --randomize --bg-fill ~/Pictures/wallpapers/* &
+
+# 3. Utility
+dunst &
+flameshot &
+
+# 4. Spesifikt for stasjonær/NVIDIA (Valgfritt)
+# Hvis du har flere skjermer, kan du legge til xrandr-oppsett her
+# xrandr --output DP-0 --primary --mode 2560x1440 --rate 144 --output HDMI-0 --right-of DP-0 &
